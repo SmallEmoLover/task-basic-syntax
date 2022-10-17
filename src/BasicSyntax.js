975f924cab23478fa2fcd9c1f48971cf62290fc1
ds.kulakov <ds.kulakov@tensor.ru>
2022-10-18 00:46:57 +0300

    https://new-edu.tensor.ru/tensor/lesson/page.php?id=3917&sectionid=10&pageid=0

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,35 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let previous = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const current = letter_to_number(str[i]);
+
+        if (current > previous) {
+            result += current - 2 * previous;
+        } else {
+            result += current;
+        }
+        previous = current;
+    }
     return result;
+}
+
+function letter_to_number(letter) {
+    switch (letter) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+    }
 }
